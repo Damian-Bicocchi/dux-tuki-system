54312925e5965b0df73bf2d7770adfb4dfcf9dae
franns008 <franns008@gmail.com>
2026-06-27 17:06:16 -0300

    Fix en el DB_Path, ubicacion incorrecta

--- backend/config/env.js
+++ backend/config/env.js
@@ -4,6 +4,6 @@ module.exports = {
   PORT: process.env.PORT || 3001,
   ADMIN_USERNAME: process.env.ADMIN_USERNAME || 'admin@gmail.com',
   ADMIN_PASSWORD: process.env.ADMIN_PASSWORD || 'Hola1234',
-  DB_PATH: process.env.DB_PATH || path.resolve(__dirname, '../BD/database.db'),
+  DB_PATH: process.env.DB_PATH || path.resolve(__dirname, '../../BD/database.db'),
   SALT_ROUNDS: 10,
 };
